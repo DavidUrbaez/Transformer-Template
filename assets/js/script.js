9cc07de30ca0264ea1295266173732c14b95bbf3
DavidUrbaez <durbaezl@unal.edu.co>
2021-06-03 15:08:02 -0500

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $("#banner-slider .owl-carousel").owlCarousel({
         items: 1,
         loop: true,
-        autoplay: true,
+        autoplay: false,
         nav: true
     });
 });
